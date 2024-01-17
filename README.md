f37f7d22c173727832611d5c9548d7093a0caecf
Allusura <110790823+Allusura@users.noreply.github.com>
2024-01-17 20:24:53 +0000

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# miles_to_km_GUI
-My first interaction with the Tkinter library
---
+++ README.py
@@ -0,0 +1,16 @@
+# miles_to_km_GUI
+# My first interaction with the Tkinter library
+
+#Things I learned today:
+# How to make a window with tkinter
+# How to make a label with tkinter
+# How to place the Label on the screen
+# How to change the title + size of the screen you make
+# How to use the .pack() layout manager
+# How to use the .place() layout manager
+# How to use the .grid() layout manager
+# (*args) = unlimited Arguments
+# (**kwargs) = Many Keyword Arguments
+# Benefit of .get() = will return "None" if it doesn't exist
+# Tkinter Widgets (Radiobuttons, Scales, Checkbuttons, etc...)
+# How to add padding (padx, pady)
